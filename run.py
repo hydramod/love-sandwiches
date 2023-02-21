ec2f1afd6bf747d08649f6ff7c3023d37598827e
Ali <alissaeid@live.co.uk>
2023-02-21 22:09:39 +0000

    Calculate stock number and update stock sheet

--- run.py
+++ run.py
@@ -93,6 +93,22 @@ def get_last_5_entries_sale():
     return columns
 
 
+def calculate_stock_data(data):
+    """
+    Calculate average stock and add 10%
+    """
+    print("Calculating stock data....\n")
+    new_stock_data = []
+
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+
+    return new_stock_data
+
+
 def main():
     """
     Run all functions
@@ -103,9 +119,9 @@ def main():
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, "surplus")
     sales_columns = get_last_5_entries_sale()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, "stock")
 
 
 print("Welcome to love sandwiches data automation")
 main()
-
-
