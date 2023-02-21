d40880053d176a7b0cb1ace94b8d07b591c26e40
Ali <alissaeid@live.co.uk>
2023-02-21 18:34:00 +0000

    Update validation function to convert str to int

--- run.py
+++ run.py
@@ -29,6 +29,7 @@ def validate_data(values):
     Raises ValueError if strings cannot be converted to int
     """
     try:
+        [int(values) for value in values]
         if len(values) != 6:
             raise ValueError(f"Exactly 6 values required, you provided {len(values)}")
     except ValueError as e:
