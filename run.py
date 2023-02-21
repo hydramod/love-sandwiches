73315397090084977a883cf5d83d5f580824f9d0
Ali <alissaeid@live.co.uk>
2023-02-21 21:54:49 +0000

    Add fucntion to get last 5 sale entries

--- run.py
+++ run.py
@@ -79,6 +79,20 @@ def calculate_surplus_data(sales_row):
     return surplus_data
 
 
+def get_last_5_entries_sale():
+    """
+    Get last 5 ales records, then returns the data as a list
+    """
+    sales = SHEET.worksheet("sales")
+
+    columns = []
+    for ind in range(1, 7):
+       column = sales.col_values(ind)
+       columns.append(column[-5:])
+
+    return columns
+
+
 def main():
     """
     Run all functions
@@ -88,7 +102,10 @@ def main():
     update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, "surplus")
+    sales_columns = get_last_5_entries_sale()
 
 
 print("Welcome to love sandwiches data automation")
 main()
+
+
